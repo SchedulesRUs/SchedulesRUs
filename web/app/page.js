0a441cbf69863f02080a05cb9ddbcec29ef8c2ef
KamikazeFelix <unchinoiszyh@gmail.com>
2024-01-25 10:16:00 -0700

    styling input box and button

--- web/app/page.js
+++ web/app/page.js
@@ -1,5 +1,4 @@
 "use client"; 
-// page.js
 
 import { useState } from 'react';
 
@@ -12,11 +11,9 @@ export default function Home() {
     const username = "Admin123";
     const password = "schedule123";
     if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
-      // Redirect to the dashboard page
       window.location.href = '/dashboard';
     } else {
-      // Display an error message or handle the invalid credentials case
-      console.error('Invalid credentials'); // Replace with a more secure error handling method
+      console.error('Invalid credentials'); 
     }
   };
 
@@ -24,7 +21,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="max-w-md w-full space-y-8">
         <div className="text-center">
-          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+          <h2 className="mt-6 text-3xl font-extrabold text-purple-800">
             Schedule "R" Us
           </h2>
         </div>
@@ -63,7 +60,7 @@ export default function Home() {
             <button
               onClick={handleLogin}
               type="button"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Login
             </button>
