fed587b1e4f08f4fa8d924e267b4341c29ebe7f5
Khit <126133522+JustKhit@users.noreply.github.com>
2024-01-25 09:32:19 -0700

    re: init a boilerplate

---
+++ mobile/src/navigators/Application.tsx
@@ -0,0 +1,25 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Example, Startup, Login } from '@/screens';
+import { useTheme } from '@/theme';
+
+import type { ApplicationStackParamList } from '@/types/navigation';
+
+const Stack = createStackNavigator<ApplicationStackParamList>();
+
+function ApplicationNavigator() {
+	const { variant, navigationTheme } = useTheme();
+
+	return (
+		<NavigationContainer theme={navigationTheme}>
+			<Stack.Navigator key={variant} screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="Startup" component={Startup} />
+				<Stack.Screen name="Example" component={Example} />
+				{/* <Stack.Screen name="Login" component={Login} /> */}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+
+export default ApplicationNavigator;
