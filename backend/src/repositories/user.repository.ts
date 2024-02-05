fae6ca2a724bdd998d7e10de5a45cd6b5b99cc4a
thungoc127 <thungoc127@gmail.com>
2024-02-05 12:02:22 -0700

    update user login back end

---
+++ backend/src/repositories/user.repository.ts
@@ -0,0 +1,11 @@
+import { Repository } from 'typeorm';
+import User from '../entities/user.entity';
+
+export class UserRepository extends Repository<User> {
+    async findAll(): Promise<User[]> {
+        return this.find();
+      }
+        async findByUsername(username: string): Promise<User | undefined> {
+    return this.findOne({ where: { username } });
+  }
+}
