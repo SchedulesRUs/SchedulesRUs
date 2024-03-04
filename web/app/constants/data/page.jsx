dfe15de3ee180c4e36b21327fc8b557a993bacf5
thungoc127 <thungoc127@gmail.com>
2024-03-03 19:41:00 -0700

    update find by userid

--- web/app/constants/data/page.jsx
+++ web/app/constants/data/page.jsx
@@ -1,47 +1,47 @@
-// "use client";
-// import { useState, useEffect } from "react";
+"use client";
+import { useState, useEffect } from "react";
 
-// export default function getAllUser() {
-//   useEffect(() => {
-//     async function fetchGetAllUser() {
-//       try {
-//         const response = await fetch(`https://schedules-r-us-78b737cd078f.herokuapp.com/user`);
-//         const data = await response.json();
-//         console.log("test", data);
-//         console.dir(data);
-//         setAllUser(data);
-//         setErrorStatus(false);
-//       } catch (error) {
-//         setErrorStatus(true);
-//       }
-//     }
+export default function getAllUser() {
+  useEffect(() => {
+    async function fetchGetAllUser() {
+      try {
+        const response = await fetch(`https://schedules-r-us-78b737cd078f.herokuapp.com/user`);
+        const data = await response.json();
+        console.log("test", data);
+        console.dir(data);
+        setAllUser(data);
+        setErrorStatus(false);
+      } catch (error) {
+        setErrorStatus(true);
+      }
+    }
 
-//     fetchGetAllUser();
-//   }, []);
+    fetchGetAllUser();
+  }, []);
 
-//   const [allUser, setAllUser] = useState([]);
-//   const [isError, setErrorStatus] = useState(false);
+  const [allUser, setAllUser] = useState([]);
+  const [isError, setErrorStatus] = useState(false);
 
-//   return (
-//     <div>
-//       <h1>Meal Ideas</h1>
-//       <h1>Here are some meal ideas using :</h1>
+  return (
+    <div>
+      <h1>Meal Ideas</h1>
+      <h1>Here are some meal ideas using :</h1>
 
-//       <div className="ml-8 p-2">
-//         {isError ? (
-//           <p>Error fetching data</p>
-//         ) : allUser.length > 0 ? (
-//           <ul>
-//             {allUser.map((item) => (
-//               <li key={item.id} className="p-2 m-1 hover:bg-orange-800 cursor-pointer">
-//                 {item.username}
-//               </li>
-//             ))}
-//           </ul>
-//         ) : (
-//           <p>Loading User</p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
+      <div className="ml-8 p-2">
+        {isError ? (
+          <p>Error fetching data</p>
+        ) : allUser.length > 0 ? (
+          <ul>
+            {allUser.map((item) => (
+              <li key={item.id} className="p-2 m-1 hover:bg-orange-800 cursor-pointer">
+                {item.username}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Loading User</p>
+        )}
+      </div>
+    </div>
+  );
+}
