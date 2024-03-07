90bd5b7878e75089d9213150b85ae1b55279505e
donnysait88 <119543204+donnysait88@users.noreply.github.com>
2024-03-07 10:45:14 -0700

    Add on create/update DTO

---
+++ backend/src/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+export class CreateUserDto {
+    id: number;
+    username: string;
+    password: string;
+    email: string;
+    userColor: string;
+    phone: string;
+    isAdmin: string;
+    role: string;
+    address: string;
+    image: string;
+  }
+  
