3754cc47d95fe345afc32f34f994e487d08faeb6
donnysait88 <119543204+donnysait88@users.noreply.github.com>
2024-03-29 23:06:44 -0600

    Update Config.jsx

--- web/app/constants/Config.jsx
+++ web/app/constants/Config.jsx
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://schedules-r-us-78b737cd078f.herokuapp.com";
-export const BASE_URL = 'http://localhost:5001'
+export const BASE_URL = "https://schedules-r-us-78b737cd078f.herokuapp.com";
+// export const BASE_URL = 'http://localhost:5001'
